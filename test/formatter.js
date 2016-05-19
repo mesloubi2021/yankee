542b3965114f10e8ec8b6b4e2a8b7dd787da6f24
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-19 16:50:40 +0200

    Improve test formatting

--- test/formatter.js
+++ test/formatter.js
@@ -16,25 +16,25 @@ console.log = function consoleLog() {
   }
 
   const otherArgs = Array.prototype.slice.call(arguments, 1);
-  if (message === 'TAP version 13' && !otherArgs.length) {
+  if ((
+    message === 'TAP version 13' ||
+    plan.test(message)
+  ) && !otherArgs.length) {
     return originalConsoleLog.apply(console, ['']);
   }
 
   const reformattedMessage = (
-    (plan.test(message) &&
-      ' '
-    ) ||
     (footer.test(message) &&
       chalk.bold(message.replace(comment, ''))
     ) ||
     (comment.test(message) &&
-      chalk.bold(message.replace(comment, '\n\n'))
+      `\n${chalk.bold(message.replace(comment, ''))}\n`
     ) ||
     (ok.test(message) &&
-      message.replace(ok, `\n  ${chalk.green('✔')} `)
+      message.replace(ok, `  ${chalk.green('✔')} `)
     ) ||
     (notOk.test(message) &&
-      chalk.red(message.replace(notOk, `\n  ✘ `))
+      chalk.red(message.replace(notOk, '  ✘ '))
     ) ||
     chalk.dim(message)
   );
