87a1419a158cff0681fe6cbf925a58ea711d6099
Udit Vasu <udit@pm.me>
2021-04-25 14:20:26 +0530

    Support `unreleased:` top-level key and case-insensitive file name

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-'use strict'; // eslint-disable-line strict
-  // http://stackoverflow.com/q/33063206
-
 const fs = require('fs');
 const yaml = require('js-yaml');
 const tinyError = require('tiny-error');
@@ -8,7 +5,10 @@ const isObject = require('isobject');
 const omit = require('object.omit');
 const includes = require('array-includes');
 const dateFormat = require('date-format');
-const spawnSync = require('child_process').spawnSync;
+const { spawnSync } = require('child_process');
+
+// (Object, String) => Boolean
+const has = (obj, key) => Object.hasOwnProperty.call(obj, key);
 
 // (String) => Error
 const prettyError = (message) => (
@@ -26,15 +26,19 @@ const nextVersion = (previousVersion, bump) => {
 
   const versionNumbers = previousVersion.split('.').map(Number);
   return (
-    (bump === 'breaking' &&
-      `${versionNumbers[0] + 1}.0.0`
-    ) ||
-    (bump === 'feature' &&
-      `${versionNumbers[0]}.${versionNumbers[1] + 1}.0`
-    ) ||
-    `${versionNumbers[0]}.${versionNumbers[1]}.${versionNumbers[2] + 1}`
+    (bump === 'breaking'
+      && `${versionNumbers[0] + 1}.0.0`
+    )
+    || (bump === 'feature'
+      && `${versionNumbers[0]}.${versionNumbers[1] + 1}.0`
+    )
+    || `${versionNumbers[0]}.${versionNumbers[1]}.${versionNumbers[2] + 1}`
   );
 };
+
+// (String) => String || null
+const changelogFileName = (path) => fs.readdirSync(path)
+  .find((file) => /changelog.y[a]?ml/.test(file.toLowerCase()));
 
 /*                                                            (see git.io/rtype)
   ({
@@ -48,8 +52,8 @@ const nextVersion = (previousVersion, bump) => {
       // Stream to write messages to
 
     npm = false: Boolean,
-      // If `true`, we’ll update the `version` in the `package.json`
-      // and `npm-shrinkwrap.json`
+      // If `true`, we’ll update the `version` in the `package.json`,
+      // `package-lock.json` and `npm-shrinkwrap.json` (whichever are present)
 
     commit = false: Boolean,
       // If `true`, we’ll commit the results with git
@@ -71,42 +75,42 @@ module.exports = (paramsArg) => {
   const commit = params.commit || params.tag || false;
   const tag = params.tag || false;
 
-  const changelogPath = `${path}/Changelog.yaml`;
-  const changelog = yaml.safeLoad(fs.readFileSync(changelogPath, 'utf8'));
+  const changelogFile = changelogFileName(path);
+  const changelogPath = `${path}/${changelogFile}`;
+  const changelog = yaml.load(fs.readFileSync(changelogPath, 'utf8'));
 
   if (!isObject(changelog)) {
     throw prettyError('Make sure `Changelog.yaml` is a YAML object.');
   }
 
-  if (!changelog.hasOwnProperty('master')) {
+  const unreleasedTitle = has(changelog, 'unreleased') ? 'unreleased'
+    : (has(changelog, 'master') && 'master');
+
+  if (!unreleasedTitle) {
     throw prettyError(
-      'Make sure you have a top-level `master:` property in your ' +
-      '`Changelog.yaml`.'
+      'Make sure you have a top-level `unreleased:` property in your '
+      + '`Changelog.yaml`.',
     );
   }
 
-  const changelogWithoutMaster = omit(changelog, 'master');
-  const masterData = changelog.master;
-  const masterDataKeys = Object.keys(masterData);
-  const previousVersion = Object.keys(changelogWithoutMaster)[0];
+  const releasedChangelog = omit(changelog, unreleasedTitle);
+  const unreleasedData = changelog[unreleasedTitle];
+  const unreleasedDataKeys = Object.keys(unreleasedData);
+  const previousVersion = Object.keys(releasedChangelog)[0];
   const bump = (
-    (!previousVersion && 'initial') ||
-    (includes(masterDataKeys, 'breaking changes') && 'breaking') ||
-    (includes(masterDataKeys, 'new features') && 'feature') ||
-    'bugfix'
+    (!previousVersion && 'initial')
+    || (includes(unreleasedDataKeys, 'breaking changes') && 'breaking')
+    || (includes(unreleasedDataKeys, 'new features') && 'feature')
+    || 'bugfix'
   );
   const newVersion = nextVersion(previousVersion, bump);
 
-  const newVersionData = Object.assign({
-    date: dateFormat('yyyy-MM-dd', date),
-  }, masterData);
-  const newChangelog = Object.assign({
-    [newVersion]: newVersionData,
-  }, changelogWithoutMaster);
-  const newChangelogString = yaml.safeDump(newChangelog)
+  const newVersionData = { date: dateFormat('yyyy-MM-dd', date), ...unreleasedData };
+  const newChangelog = { [newVersion]: newVersionData, ...releasedChangelog };
+  const newChangelogString = yaml.dump(newChangelog)
     // Reformat dates
     .replace(/(^\s*date:\s*)'(.*)'$/mg, '$1$2')
-    .replace(/(^\s*date:\s*)(\d{4}-\d{2}-\d{2})[tT][\d:\.+zZ]+$/mg, '$1$2')
+    .replace(/(^\s*date:\s*)(\d{4}-\d{2}-\d{2})[tT][\d:.+zZ]+$/mg, '$1$2')
     // Add more air
     .replace(/(.)\n([^\s])/g, '$1\n\n$2');
 
@@ -127,35 +131,35 @@ module.exports = (paramsArg) => {
       data = JSON.parse(fileContents);
     } catch (error) {
       if (error instanceof SyntaxError) throw prettyError(
-        `Make sure \`${filename}\` is valid JSON.`
+        `Make sure \`${filename}\` is valid JSON.`,
       );
     }
     if (typeof data !== 'object' || data === null) throw prettyError(
-      `Make sure \`${filename}\` is a JSON object.`
+      `Make sure \`${filename}\` is a JSON object.`,
     );
 
     data.version = newVersion;
     fs.writeFileSync(
       `${path}/${filename}`,
-      `${JSON.stringify(data, null, '  ')}\n`
+      `${JSON.stringify(data, null, '  ')}\n`,
     );
 
     return filename;
   };
 
   const jsonFilesToUpdate = (npm
-    ? ['package.json', 'npm-shrinkwrap.json']
+    ? ['package.json', 'npm-shrinkwrap.json', 'package-lock.json']
     : []
   );
   const updatedFiles = jsonFilesToUpdate
     .map(tryUpdatingFile)
-    .filter(filename => filename !== null);
+    .filter((filename) => filename !== null);
 
   if (commit) {
     const args = [
-      'commit', `--message=${newVersion}`, 'Changelog.yaml',
+      'commit', `--message=${newVersion}`, changelogFile,
     ].concat(
-      updatedFiles
+      updatedFiles,
     );
 
     process.stdout.write(`\n❭ git ${args.join(' ')}\n`);
